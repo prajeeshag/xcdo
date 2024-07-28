54c9e3877769d789ae34bc22f14649fcc880835b
Prajeesh Ag <prajeeshag@gmail.com>
2024-07-28 18:47:03 +0300

    renaming complete

--- src/xcdo/core/cli/operation.py
+++ src/xcdo/core/cli/operation.py
@@ -2,11 +2,11 @@ from .argument.tokens import OperatorToken
 from .registry import OperatorRegistry
 
 
-class Operations:
+class Operation:
     """
-    Operations class takes an OperatorToken and OperatorRegistry
+    Operation class takes an OperatorToken and OperatorRegistry
     and sets the operator and its arguments. (and its children recursively
-    which are again Operations)??
+    which are again Operation)??
     """
 
     def __init__(self, token: OperatorToken, registry: OperatorRegistry) -> None:
