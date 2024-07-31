ae5848654ce61180cb055bb47e4481d2f23a6ec1
Prajeesh <prajeeshag@gmail.com>
2024-07-31 10:09:43 +0300

    Implementing Reader

--- src/xcdo/core/cli/operator/_Reader.py
+++ src/xcdo/core/cli/operator/_Reader.py
@@ -1,11 +1,10 @@
+from dataclasses import dataclass
 from typing import Any, Callable
 
 from ..exceptions import InvalidFunction
 from ._Converter import Converter
 
 
-class Reader(Converter):
-    def __init__(self, fn: Callable[[str], Any]) -> None:
-        super().__init__(fn)
-        if self.input_type is not str:
-            raise InvalidFunction("Input should be of type <str>", self._fn)
+@dataclass(frozen=True)
+class Reader:
+    pass
