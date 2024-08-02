358b8be3932be665cce10f80b1d2be671aada007
Prajeesh <prajeeshag@gmail.com>
2024-08-02 22:32:16 +0300

    Implementing Registry

--- src/xcdo/core/cli/registry/_IRegistry.py
+++ src/xcdo/core/cli/registry/_IRegistry.py
@@ -1,12 +1,22 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Hashable
+
+from ..operator import BaseOperator, Reader, Writer
 
 
 class IRegistry(ABC):
     @abstractmethod
-    def get(self, key: Any) -> Any:
+    def get(self, key: Hashable) -> BaseOperator | Writer | Reader:
         """
         Returns:
             None: if `key` not present
+        """
+        pass
+
+    @abstractmethod
+    def add(self, key: Hashable, obj: Any) -> None:
+        """
+        Raises:
+            EntryExistError if key already exist
         """
         pass
--- src/xcdo/core/cli/registry/_OperatorRegistry.py
+++ src/xcdo/core/cli/registry/_OperatorRegistry.py
@@ -1,5 +1,16 @@
+from typing import Hashable
+
+from xcdo.core.cli.operator._Reader import Reader
+
+from ..operator import BaseOperator, Writer
 from ._IRegistry import IRegistry
 
 
 class OperatorRegistry(IRegistry):
-    pass
+    _db: dict[str, BaseOperator | Writer]
+
+    def get(self, key: Hashable) -> BaseOperator | Writer:
+        raise NotImplementedError
+
+    def add(self, key: Hashable, obj: BaseOperator | Writer) -> None:
+        raise NotImplementedError
