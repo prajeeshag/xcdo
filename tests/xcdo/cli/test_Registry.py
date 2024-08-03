e742c8b332718238bff00691d6a9b214d1ac9b16
Prajeesh <prajeeshag@gmail.com>
2024-08-03 22:24:10 +0300

    updated test_Registry

--- tests/xcdo/cli/test_Registry.py
+++ tests/xcdo/cli/test_Registry.py
@@ -3,19 +3,19 @@ from xcdo.cli.registry import KeyExistsError, Registry
 
 
 def test_set_get():
-    reg = Registry()
+    reg = Registry[str, str]()
     reg.set("k", "object")
     assert reg.get("k") == "object"
 
 
 def test_get_none():
-    reg = Registry()
+    reg = Registry[str, str]()
     with pytest.raises(KeyError):
         reg.get("k")
 
 
 def test_set_key_exist():
-    reg = Registry()
+    reg = Registry[str, str]()
     reg.set("k", "object")
     with pytest.raises(KeyExistsError) as e:
         reg.set("k", "object")
