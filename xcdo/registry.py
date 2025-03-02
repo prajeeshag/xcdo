10da53444a568249b559b3edb0c1b134ec1b52df
prajeesh <prajeeshag@gmail.com>
2025-03-02 20:07:53 +0300

    feat: adds cdo, showtimestamp operators

--- xcdo/registry.py
+++ xcdo/registry.py
@@ -2,7 +2,9 @@ from clios import OperatorFns
 
 
 def load_operator_registry() -> OperatorFns:
+    from .operators import cdo as cdo
     from .operators import fn_registry
+    from .operators import info as info
     from .operators import misc as misc
     from .operators import missing_values as missing_values
     from .operators import plotting as plotting
